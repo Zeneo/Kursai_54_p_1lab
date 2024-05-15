4269f2df0de953a309e7fd982bf36b6a3e350a3d
PauliusNavickas <paulius.zee@gmail.com>
2024-05-15 11:02:17 +0300

    Finished

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,9 +1,9 @@
 
 let budget = 0;
 let time = 0;
-let timeMin = 2000;
-let timeMax = 5000;
-let timeUntil = 3000;
+let timeMin = 200;
+let timeMax = 3000;
+let timeUntil = 0;
 let boxes = document.getElementById('js-main').querySelectorAll('.js-square');
 let active = 0;
 let actCnt = '';
@@ -13,9 +13,7 @@ let d = {
     'top': 0,
     'left': 0
 };
-const interval = setTimeout(function() {
-    generate();
-}, timeUntil);
+generate();
 
 
 function generate() {
@@ -32,14 +30,16 @@ function generate() {
     d.left = getRandom(0, d.cntSize-d.stickerSize);
     actCnt = document.getElementById('square_'+active).innerHTML = '<div id="actCnt" ' +
         'style="top: '+d.top+'px; left: '+d.left+'px; height: '+d.stickerSize+'px; width: '+d.stickerSize+'px;"></div>';
+    timeUntil = getRandom(timeMin, timeMax);
+    let point = setTimeout(function() {
+        generate();
+    }, timeUntil);
     document.getElementById('actCnt').addEventListener('click', function(e) {
+        clearTimeout(point);
+        generate();
         document.getElementById('js-counter').querySelector('span').innerHTML
             = parseInt(document.getElementById('js-counter').querySelector('span').innerText)+1;
     })
-    timeUntil = getRandom()
-    setTimeout(function() {
-        generate();
-    }, timeUntil);
 }
 
 function getRandom(min, max) {
